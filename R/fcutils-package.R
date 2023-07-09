d3e2a14ff11841afffe48234fac28764a4ae7a3f
fuleky <fuleky@hawaii.edu>
2023-07-09 21:46:49 +0200

    Initial commit

---
+++ R/fcutils-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %>%
+#' @importFrom rlang :=
+#' @importFrom rlang !!
+#' @importFrom rlang sym
+#' @importFrom tsbox %ts-%
+#' @importFrom tsbox %ts*%
+#' @importFrom tsbox %ts/%
+#' @importFrom tsbox %ts+%
+## usethis namespace: end
+NULL
---
+++ R/pkg_data.R
@@ -0,0 +1,26 @@
+# # define project-wide constants
+# # bank start
+# bnk_start <- lubridate::ymd("1900-01-01")
+# # usethis::use_data(bnk_start)
+# # bank end
+# bnk_end <- lubridate::ymd("2060-12-31")
+# # usethis::use_data(bnk_end)
+# # colors defined in the UHERO Style Guide
+# uhero_colors <- c("#1D667F", "#F6A01B", "#9BBB59", "#8064A2", "#7EC4CA", "#505050", "red")
+# # usethis::use_data(uhero_colors)
+# # oi <- uhero_colors
+# # colorspace::swatchplot(
+# #   "-60%" = colorspace::lighten(oi, 0.6),
+# #   "-40%" = colorspace::lighten(oi, 0.4),
+# #   "-20%" = colorspace::lighten(oi, 0.2),
+# #   "  0%" = oi,
+# #   " 20%" =  colorspace::darken(oi, 0.2),
+# #   " 40%" =  colorspace::darken(oi, 0.4),
+# #   off = c(0, 0)
+# # )
+# uhero_colors_light <- c("#90C3DC", "#FFD9BD", "#CAEA8D", "#D2B8F6", "#AAEEF4", "#B5B5B5", "#FFB8B8")
+# # usethis::use_data(uhero_colors_light)
+# # colorspace::adjust_transparency(uhero_colors_light,   alpha = 0.5)
+# uhero_colors_50 <- c("#1D667F80", "#F6A01B80", "#9BBB5980", "#8064A280", "#7EC4CA80", "#50505080", "#FF000080")
+# # usethis::use_data(uhero_colors_50)
+# usethis::use_data(bnk_start, bnk_end, uhero_colors, uhero_colors_light, uhero_colors_50, internal = TRUE)
